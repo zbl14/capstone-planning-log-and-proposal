76521dda0f1de40ba6fce08d1e45c5a9eae1c6c1
zbl14 <ifthereisoneday@gmail.com>
2022-09-23 08:42:06 -0700

    add form to submit term and location

--- src/components/landingPage/searchBar/SearchBar.js
+++ src/components/landingPage/searchBar/SearchBar.js
@@ -1,15 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const [term, setTerm] = useState(props.term || "");
+  const [location, setLocation] = useState(props.location || "");
+
+  const submit = (event) => {
+    props.search(term, location);
+    event.preventDefault();
+  };
   return (
     <React.Fragment>
-      <h1>This is a SearchBar, a form</h1>
-      <Link to="/search">
-        <button submit>Search</button>
-      </Link>
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          name="term"
+          onChange={(event) => {
+            setTerm(event.target.value);
+          }}
+          value={term}
+        />
+        <input
+          type="text"
+          name="location"
+          onChange={(event) => {
+            setLocation(event.target.value);
+          }}
+          value={location}
+        />
+        <button onClick={submit}>Search</button>
+      </form>
     </React.Fragment>
   );
+};
+
+SearchBar.propTypes = {
+  search: PropTypes.func,
 };
 
 export default SearchBar;
