322074d9cb158a7ae870fb26ff00d8e46ed550e9
zbl14 <ifthereisoneday@gmail.com>
2022-09-23 09:55:36 -0700

    test and write useBusinessSearchReducer

--- src/__tests__/reducers/use-business-research-reducer.test.js
+++ src/__tests__/reducers/use-business-research-reducer.test.js
@@ -1,17 +1,55 @@
 import useBusinessSearchReducer from "../../reudcers/use-business-search-reducer";
+import * as c from "./../../actions/ActionTypes";
 
 describe("useBusinessSearchReducer", () => {
+  let action;
   const initialState = {
     error: null,
     isLoaded: false,
     businesses: [],
     amountResults: 0,
-    searchParams: { term: "burger", location: "tustin" },
+    searchParams: { term: "", location: "" },
   };
 
   test("should succesfully throw a new error if a non-matching action type is passed into it", () => {
     expect(() => {
       useBusinessSearchReducer(initialState, { type: null });
     }).toThrowError("There is no action matching null.");
+  });
+
+  test("successfully getting businesses should change isLoaded to true and update business, amountResults and searchParams", () => {
+    const businesses = "List of businesses";
+    const amountResults = 1000;
+    const searchParams = { term: "burger", location: "irvine" };
+    action = {
+      type: c.GET_BUSINESS_SUCCESS,
+      businesses,
+      amountResults,
+      searchParams,
+    };
+
+    expect(useBusinessSearchReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      businesses: "List of businesses",
+      amountResults: 1000,
+      searchParams: { term: "burger", location: "irvine" },
+      error: null,
+    });
+  });
+
+  test("failing to get buesiness should change isLoaded to true and add an error message", () => {
+    const error = "An error";
+    action = {
+      type: c.GET_BUSINESS_FAILURE,
+      error,
+    };
+
+    expect(useBusinessSearchReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      businesses: [],
+      amountResults: 0,
+      searchParams: { term: "", location: "" },
+      error: "An error",
+    });
   });
 });
--- src/reudcers/use-business-search-reducer.js
+++ src/reudcers/use-business-search-reducer.js
@@ -1,5 +1,21 @@
+import * as c from "../actions/ActionTypes";
+
 const useBusinessSearchReducer = (state, action) => {
   switch (action.type) {
+    case c.GET_BUSINESS_SUCCESS:
+      return {
+        ...state,
+        isLoaded: true,
+        businesses: action.businesses,
+        amountResults: action.amountResults,
+        searchParams: action.searchParams,
+      };
+    case c.GET_BUSINESS_FAILURE:
+      return {
+        ...state,
+        isLoaded: true,
+        error: action.error,
+      };
     default:
       throw new Error(`There is no action matching ${action.type}.`);
   }
