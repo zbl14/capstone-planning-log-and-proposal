3d62fac6dc7c3a2c77267a4c77cb7301208ee5af
zbl14 <ifthereisoneday@gmail.com>
2022-09-23 10:59:31 -0700

    implemented useBusinessSearchReducer

--- src/hooks/yelpAPI/useBusinessSearch.js
+++ src/hooks/yelpAPI/useBusinessSearch.js
@@ -1,12 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import queryString from "query-string";
 import { API_BASE_URL, BEARER_TOKEN } from "./config";
+import useBusinessSearchReducer from "../../reudcers/use-business-search-reducer";
+import { getBusinessSuccess, getBusinessFailure } from "../../actions/index";
 
 const useBusinessSearch = (term, location) => {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [businesses, setBusinesses] = useState([]);
-  const [amountResults, setAmountResults] = useState();
+  const initialState = {
+    isLoaded: false,
+    error: null,
+    businesses: [],
+    amountResults: 0,
+  };
+
+  const [state, dispatch] = useReducer(useBusinessSearchReducer, initialState);
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [businesses, setBusinesses] = useState([]);
+  // const [amountResults, setAmountResults] = useState();
   const [searchParams, setSearchParams] = useState({ term, location });
 
   useEffect(() => {
@@ -27,20 +37,30 @@ const useBusinessSearch = (term, location) => {
         if (!response.ok) {
           throw new Error(`${response.status}: ${response.statusText}`);
         } else {
-          console.log(response);
+          // console.log(response);
           const jsonResponse = await response.json();
-          // console.log(jsonResponse);
-          setBusinesses(jsonResponse.businesses);
-          setAmountResults(jsonResponse.total);
-          setIsLoaded(true);
+          console.log(jsonResponse);
+          const action = getBusinessSuccess(
+            jsonResponse.businesses,
+            jsonResponse.total
+          );
+          dispatch(action);
+          // setBusinesses(jsonResponse.businesses);
+          // setAmountResults(jsonResponse.total);
+          // setIsLoaded(true);
         }
       } catch (error) {
-        setError(error);
-        setIsLoaded(true);
+        const action = getBusinessFailure(error.message);
+        dispatch(action);
+        // setError(error);
+        // setIsLoaded(true);
       }
     };
     fetchData();
   }, [searchParams]);
+
+  const { error, isLoaded, businesses, amountResults } = state;
+
   return [
     error,
     isLoaded,
