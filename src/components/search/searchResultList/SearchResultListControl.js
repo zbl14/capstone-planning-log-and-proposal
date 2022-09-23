76a01a441d85d279991c88ce684fab3366b91796
zbl14 <ifthereisoneday@gmail.com>
2022-09-23 11:56:27 -0700

    Add SearchResultList and SearchResultDetail

--- src/components/search/searchResultList/SearchResultListControl.js
+++ src/components/search/searchResultList/SearchResultListControl.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchResultList from "./SearchResultList";
+import SearchResultDetail from "./searchResultDetail/SearchResultDetails";
 
-const SearchResultListControl = () => {
+const SearchResultListControl = (props) => {
+  const [selectedSearchResult, setSelectedSearchResult] = useState(null);
+
+  const handleClick = () => {
+    if (selectedSearchResult != null) {
+      setSelectedSearchResult(null);
+    }
+  };
+
+  let curVisibleState;
+  let buttonText = null;
+
+  if (selectedSearchResult != null) {
+    curVisibleState = <SearchResultDetail />;
+    buttonText = "Return to businesses list";
+  } else {
+    curVisibleState = <SearchResultList business={props} />;
+    buttonText = "home";
+  }
+
   return (
     <React.Fragment>
       <h1>This is a SearchResultListControl</h1>
+      {curVisibleState}
+      <button onClick={handleClick}>{buttonText}</button>
       <hr />
     </React.Fragment>
   );
