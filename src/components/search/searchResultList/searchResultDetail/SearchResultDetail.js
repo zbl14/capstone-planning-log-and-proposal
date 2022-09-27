b013f294c4e80c1b14744f01b7f9fc28b1e5f4b5
zbl14 <ifthereisoneday@gmail.com>
2022-09-26 23:08:30 -0700

    add CommentList and NewCommentForm

--- src/components/search/searchResultList/searchResultDetail/SearchResultDetail.js
+++ src/components/search/searchResultList/searchResultDetail/SearchResultDetail.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import CommentList from "./CommentList";
+import NewCommentForm from "./NewCommentForm";
 
 const SearchResultDetail = (props) => {
-  console.log(props);
-  return <h1>this is Search Result Detail</h1>;
+  const business = props.business;
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [mainCommentList, setMainCommentList] = useState([]);
+
+  const handleClick = () => {
+    setFormVisibleOnPage(!formVisibleOnPage);
+  };
+
+  const handleAddingNewCommentToList = (newComment) => {
+    const newMainCommentList = mainCommentList.concat(newComment);
+    setMainCommentList(newMainCommentList);
+    setFormVisibleOnPage(false);
+  };
+
+  let curVisibleState = null;
+  let buttonText = null;
+
+  if (formVisibleOnPage) {
+    curVisibleState = (
+      <NewCommentForm onNewCommentCreation={handleAddingNewCommentToList} />
+    );
+    buttonText = "Return to Comment List";
+  } else {
+    curVisibleState = <CommentList commentList={mainCommentList} />;
+    buttonText = "Add Comment";
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Reviews of {business.name}</h1>
+      {curVisibleState}
+      <button onClick={handleClick}>{buttonText}</button>
+    </React.Fragment>
+  );
 };
 
 export default SearchResultDetail;
