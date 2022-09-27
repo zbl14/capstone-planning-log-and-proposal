f627a87b13f133d6a46af9b4a4657f14005fe02a
zbl14 <ifthereisoneday@gmail.com>
2022-09-26 22:47:25 -0700

    remove h1

--- src/components/search/searchResultSummary/SearchResultSummary.js
+++ src/components/search/searchResultSummary/SearchResultSummary.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 const SearchResultSummary = (props) => {
   return (
     <React.Fragment>
-      <h1>This is a SearchResultSummary</h1>
       <p>
         {props.term} in {props.location}
       </p>
