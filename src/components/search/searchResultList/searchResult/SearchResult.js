eb011b850741f629f5ce8d1c581e2352aea12fb2
zbl14 <ifthereisoneday@gmail.com>
2022-09-27 12:32:37 -0700

    refactor with mui

--- src/components/search/searchResultList/searchResult/SearchResult.js
+++ src/components/search/searchResultList/searchResult/SearchResult.js
@@ -1,36 +1,128 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {
+  styled,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Collapse,
+  Avatar,
+  IconButton,
+  Typography,
+  Rating,
+  Chip,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import DoneIcon from "@mui/icons-material/Done";
+// import { red } from "@mui/material/colors";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const SearchResult = (props) => {
   const business = props.business;
+  const [expanded, setExpanded] = React.useState(false);
 
   const tags = business.categories.map((category) => (
-    <span key={business.id + category.title}>{category.title}</span>
+    <Chip
+      key={business.id + category.title}
+      label={`# ${category.title}`}
+      variant="outlined"
+    />
+  ));
+
+  const transactions = business.transactions.map((transaction) => (
+    <p key={business.id + transaction}>{transaction}</p>
   ));
 
   const addressLines = business.location.display_address.map((addressLine) => (
-    <span key={business.id + addressLine}>{addressLine}</span>
+    <p key={business.id + addressLine}>{addressLine}</p>
   ));
 
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <React.Fragment>
+    <Card sx={{ maxWidth: 345 }}>
       <div onClick={() => props.whenSearchResultClicked(business.id)}>
-        <img src={business.image_url} alt="item" />
-        <p>Busines Name{business.name}</p>
-        <p>Review Count:{business.review_count}</p>
-        <p>Rating: {business.rating}</p>
-        <p>
-          {business.price} {tags}
-        </p>
-        <p>Business Phone: {business.phone}</p>
-        <p>Busines Address: {addressLines}</p>
+        <CardHeader
+          avatar={
+            <Avatar sx={{}} alt={business.name} src="#" aria-label="business" />
+          }
+          title={business.name}
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image={business.image_url}
+          alt={business.name}
+        />
+        <CardContent>
+          <div>
+            <Rating
+              name="rating"
+              value={business.rating}
+              precision={0.5}
+              // size="large"
+              readOnly
+            />
+            <p>{business.review_count} Reviews</p>
+          </div>
+          <p>
+            {business.location.city} · {business.price}
+          </p>
+          <div>
+            <DoneIcon color="success" />
+            <p>{transactions}</p>
+          </div>
+          <div>{tags}</div>
+        </CardContent>
       </div>
-    </React.Fragment>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>{business.name}</Typography>
+          <Typography paragraph>
+            Business Phone: {business.display_phone}
+          </Typography>
+          <Typography paragraph>Busines Address: {addressLines}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
   );
 };
 
 SearchResult.propTypes = {
   business: PropTypes.object,
+  whenSearchResultClicked: PropTypes.func,
 };
 
 export default SearchResult;
