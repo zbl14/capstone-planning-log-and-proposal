5d769239c3ef3127efdc9c0c1bd52c1b76ec35fe
zbl14 <ifthereisoneday@gmail.com>
2022-09-26 20:30:52 -0700

    add SearchBar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import SearchBar from "../landingPage/searchBar/SearchBar";
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <React.Fragment>
       <h1>This is a NavBar</h1>
+      <SearchBar
+        term={props.term}
+        location={props.location}
+        search={props.search}
+      />
+      <button>Sign In</button>
+      <button>Sign Up</button>
       <hr />
     </React.Fragment>
   );
