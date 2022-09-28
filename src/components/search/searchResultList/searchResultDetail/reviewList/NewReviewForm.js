a4a924516e04726b7831d2952bb9a5ee7766a785
zbl14 <ifthereisoneday@gmail.com>
2022-09-28 13:31:09 -0700

    remove uuid

--- src/components/search/searchResultList/searchResultDetail/reviewList/NewReviewForm.js
+++ src/components/search/searchResultList/searchResultDetail/reviewList/NewReviewForm.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { v4 } from "uuid";
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
 
@@ -14,7 +13,6 @@ const NewReviewForm = (props) => {
       businessName: business.name,
       voteCount: 0,
       businessId: business.id,
-      // id: v4(),
     });
   };
   return (
