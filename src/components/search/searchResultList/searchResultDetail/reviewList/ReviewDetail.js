747689e1966ab45f16bf5dbb6a275d1bed0bf015
zbl14 <ifthereisoneday@gmail.com>
2022-09-27 22:12:58 -0700

    show comment, vote count

--- src/components/search/searchResultList/searchResultDetail/reviewList/ReviewDetail.js
+++ src/components/search/searchResultList/searchResultDetail/reviewList/ReviewDetail.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const ReviewDetail = () => {
+const ReviewDetail = (props) => {
+  const { review } = props;
   return (
     <React.Fragment>
-      <h1>review detail</h1>
+      <h1>Reveiw detail</h1>
+      <h3>Comment: {review.comment}</h3>
+      <h4>Vote Count: {review.voteCount}</h4>
     </React.Fragment>
   );
 };
