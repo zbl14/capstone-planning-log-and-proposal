069f002a4b2397a1dda8dee747df0a36fc9d358d
zbl14 <ifthereisoneday@gmail.com>
2022-09-27 21:36:54 -0700

    initial ReviewDetail

---
+++ src/components/search/searchResultList/searchResultDetail/reviewList/ReviewDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReviewDetails = () => {
+  return (
+    <React.Fragment>
+      <h1>review detail</h1>
+    </React.Fragment>
+  );
+};
+
+export default ReviewDetails;
