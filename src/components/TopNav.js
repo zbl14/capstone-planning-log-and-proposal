a9a12cbef5042df1d5d2786b4fbbc4472dce8140
zbl14 <ifthereisoneday@gmail.com>
2022-10-02 21:07:44 -0700

    add NavLink, NavButton

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -21,13 +21,38 @@ const NavLink = styled.a`
   color: #fff;
   font-weight: 500;
   cursor: pointer;
+  padding-bottom: calc(0.5em - 1px);
+  padding-left: 1em;
+  padding-right: 1em;
+  padding-top: calc(0.5em - 1px);
   margin-left: 1.5rem;
+  text-decoration: none;
+  box-shadow: inset 0 0 0 0 black;
+  padding: 0 0.25rem;
+  transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+  &:hover {
+    color: black;
+    text-decoration: underline;
+    box-shadow: inset 200px 0 0 0 #fff;
+  }
 `;
 
 const NavButton = styled.button`
   font-size: 20px;
   font-weight: 500;
   margin-left: 1.5rem;
+  background-color: white;
+  border-color: #dbdbdb;
+  border-width: 1px;
+  color: #363636;
+  cursor: pointer;
+  justify-content: center;
+  padding-bottom: calc(0.5em - 1px);
+  padding-left: 1em;
+  padding-right: 1em;
+  padding-top: calc(0.5em - 1px);
+  text-align: center;
+  white-space: nowrap;
 `;
 
 const TopNav = () => {
@@ -35,6 +60,12 @@ const TopNav = () => {
     <React.Fragment>
       <TopNavStyledWrapper>
         <LeftWrapper>
+          <NavLink
+            href="https://cors-anywhere.herokuapp.com/corsdemo"
+            target="_blank"
+          >
+            CORS Enable
+          </NavLink>
           <NavLink
             href="https://github.com/zbl14/foodie-alliance.git"
             target="_blank"
@@ -50,12 +81,6 @@ const TopNav = () => {
           <NavLink href="mailto:ifthereisoneday@gmail.com">Contact US</NavLink>
         </LeftWrapper>
         <RightWrapper>
-          <NavLink
-            href="https://cors-anywhere.herokuapp.com/corsdemo"
-            target="_blank"
-          >
-            cors enable
-          </NavLink>
           <NavButton>
             <Link to="/sign-in">Sign In / Sign Out</Link>
           </NavButton>
