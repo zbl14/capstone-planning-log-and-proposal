b5b2ff17f03c5a71c3848bfe523d7f868eba7895
zbl14 <ifthereisoneday@gmail.com>
2022-10-02 23:36:29 -0700

    Add style: SearchResultSummaryWrapper, SearchSummary, SummaryText

--- src/components/search/searchResultSummary/SearchResultSummary.js
+++ src/components/search/searchResultSummary/SearchResultSummary.js
@@ -1,5 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchResultSummaryWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgb(245, 245, 245);
+  border: 1px solid rgb(230, 230, 230);
+`;
+
+const SearchSummary = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  max-width: 1200px;
+`;
+
+const SummaryText = styled.p`
+  font-size: 0.9rem;
+  text-transform: lowercase;
+  &:first-letter {
+    text-transform: uppercase;
+  }
+`;
 
 const SearchResultSummary = (props) => {
   const sortBy = (props) => {
@@ -16,13 +41,17 @@ const SearchResultSummary = (props) => {
   };
   return (
     <React.Fragment>
-      <p>
-        {props.term} in {props.location}
-      </p>
-      <p>
-        Showing 1-{props.showResults} out of {props.amountResults} results and
-        sorted by {sortBy(props)}
-      </p>
+      <SearchResultSummaryWrapper>
+        <SearchSummary>
+          <SummaryText>
+            <strong>{props.term}</strong> in {props.location}
+          </SummaryText>
+          <SummaryText>
+            Showing 1-{props.showResults} out of {props.amountResults} results
+            and sorted by {sortBy(props)}
+          </SummaryText>
+        </SearchSummary>
+      </SearchResultSummaryWrapper>
     </React.Fragment>
   );
 };
