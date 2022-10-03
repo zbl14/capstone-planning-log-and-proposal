a3cace691740eeaeb1e8d68550fcc60cc9fda202
zbl14 <ifthereisoneday@gmail.com>
2022-10-02 22:48:06 -0700

    Styling with SearchIcon, TextField, Select and Stack

--- src/components/landingPage/searchBar/SearchBar.js
+++ src/components/landingPage/searchBar/SearchBar.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import SearchIcon from "@mui/icons-material/Search";
+import { TextField, Select, MenuItem, Stack } from "@mui/material";
 
 const SearchBar = (props) => {
   const [term, setTerm] = useState(props.term || "");
@@ -13,37 +15,63 @@ const SearchBar = (props) => {
   return (
     <React.Fragment>
       <form onSubmit={submit}>
-        <input
-          type="text"
-          name="term"
-          onChange={(event) => {
-            setTerm(event.target.value);
-          }}
-          value={term}
-        />
-        <input
-          type="text"
-          name="location"
-          onChange={(event) => {
-            setLocation(event.target.value);
-          }}
-          value={location}
-        />
-        <select
-          name="sortBy"
-          onChange={(event) => {
-            setSortBy(event.target.value);
-          }}
-          defaultValue="best_match"
-        >
-          <option value="best_match" disabled>
-            Sort By
-          </option>
-          <option value="best_match">Best Match</option>
-          <option value="rating">Highest Rated</option>
-          <option value="review_count">Most Reviewd</option>
-        </select>
-        <button onClick={submit}>Search</button>
+        <Stack direction="row" alignItems="center">
+          <TextField
+            type="text"
+            name="term"
+            onChange={(event) => {
+              setTerm(event.target.value);
+            }}
+            value={term}
+            label="FIND"
+            sx={{
+              backgroundColor: "white",
+              minWidth: "300px",
+              margin: "0.5rem",
+            }}
+          />
+          <TextField
+            type="text"
+            name="location"
+            onChange={(event) => {
+              setLocation(event.target.value);
+            }}
+            value={location}
+            label="NEAR"
+            sx={{
+              backgroundColor: "white",
+              minWidth: "300px",
+              margin: "0.5rem",
+            }}
+          />
+          <Select
+            name="sortBy"
+            onChange={(event) => {
+              setSortBy(event.target.value);
+            }}
+            sx={{ backgroundColor: "white", margin: "0.5rem" }}
+            defaultValue="best_match"
+          >
+            <MenuItem value="best_match" disabled>
+              Sort By
+            </MenuItem>
+            <MenuItem value="best_match">Best Match</MenuItem>
+            <MenuItem value="rating">Highest Rated</MenuItem>
+            <MenuItem value="review_count">Most Reviewd</MenuItem>
+          </Select>
+          <SearchIcon
+            onClick={submit}
+            fontSize="large"
+            style={{
+              color: "white",
+              backgroundColor: "#bd1f1f",
+              cursor: "pointer",
+              padding: "0.75rem",
+              borderRadius: "5px",
+              margin: "0.5rem",
+            }}
+          />
+        </Stack>
       </form>
     </React.Fragment>
   );
