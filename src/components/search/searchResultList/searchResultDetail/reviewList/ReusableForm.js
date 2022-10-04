d95edb2a64d8775e9e8f8437ba5849f9cbce70c6
zbl14 <ifthereisoneday@gmail.com>
2022-10-04 13:04:26 -0700

    remove Box

--- src/components/search/searchResultList/searchResultDetail/reviewList/ReusableForm.js
+++ src/components/search/searchResultList/searchResultDetail/reviewList/ReusableForm.js
@@ -6,29 +6,27 @@ import Grid from "@mui/material/Unstable_Grid2";
 const ReusableForm = (props) => {
   return (
     <React.Fragment>
-      <Box component="form">
-        <form onSubmit={props.formSubmissionHandler}>
-          <Grid container direction="column">
-            <Input
-              type="text"
-              name="name"
-              placeholder="Your Name"
-              sx={{ mb: 3, maxWidth: 300 }}
-            />
-            <TextField
-              name="comment"
-              placeholder="Leave a review"
-              variant="outlined"
-              sx={{ mb: 3, maxWidth: 500 }}
-              multiline
-              rows={4}
-            />
-          </Grid>
-          <Button type="submit" title={props.buttonText}>
-            {props.button}
-          </Button>
-        </form>
-      </Box>
+      <form onSubmit={props.formSubmissionHandler}>
+        <Grid container direction="column">
+          <Input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            sx={{ mb: 3, maxWidth: 300 }}
+          />
+          <TextField
+            name="comment"
+            placeholder="Leave a review"
+            variant="outlined"
+            sx={{ mb: 3, maxWidth: 500 }}
+            multiline
+            rows={4}
+          />
+        </Grid>
+        <Button type="submit" title={props.buttonText}>
+          {props.button}
+        </Button>
+      </form>
     </React.Fragment>
   );
 };
