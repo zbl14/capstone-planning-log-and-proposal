fa10cb56743f7133bee374a95d54d1cef03dd4b7
zbl14 <ifthereisoneday@gmail.com>
2022-10-04 11:10:13 -0700

    style form

--- src/components/search/searchResultList/searchResultDetail/reviewList/ReusableForm.js
+++ src/components/search/searchResultList/searchResultDetail/reviewList/ReusableForm.js
@@ -1,17 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button } from "@mui/material";
+import { Button, Input, TextField, Box } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 
 const ReusableForm = (props) => {
   return (
     <React.Fragment>
-      <form onSubmit={props.formSubmissionHandler}>
-        <input type="text" name="name" placeholder="Your Name" />
-        <textarea name="comment" placeholder="Leave a review" />
-        <Button type="submit" title={props.buttonText}>
-          {props.button}
-        </Button>
-      </form>
+      <Box component="form">
+        <form onSubmit={props.formSubmissionHandler}>
+          <Grid container direction="column">
+            <Input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              sx={{ mb: 3, maxWidth: 300 }}
+            />
+            <TextField
+              name="comment"
+              placeholder="Leave a review"
+              variant="outlined"
+              sx={{ mb: 3, maxWidth: 500 }}
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Button type="submit" title={props.buttonText}>
+            {props.button}
+          </Button>
+        </form>
+      </Box>
     </React.Fragment>
   );
 };
